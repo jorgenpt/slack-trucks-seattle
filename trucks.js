ab15a8ce8b2130d42d7ed07572ad7be41c93c1ea
Jørgen P. Tjernø <jorgen@uberent.com>
2015-09-30 10:56:47 -0700

    Add EnvelopVR endpoint & don't post on weekends.

--- trucks.js
+++ trucks.js
@@ -1,24 +1,32 @@
 var $ = require('cheerio');
 var request = require('request')
 
-var SLACK_ENDPOINT = '***REMOVED***';
-var SCHEDULE_URL = 'http://bellevue.com/food-truck.php';
-var DAY_MAPPING = [
-	'Sunday',
-	'Monday',
-	'Tuesday',
-	'Wednesday',
-	'Thursday',
-	'Friday',
-	'Saturday'
+var SLACK_ENDPOINTS = [
+	'***REMOVED***',
+	'***REMOVED***',
 ];
+
+var SCHEDULE_URL = 'http://bellevue.com/food-truck.php';
+
+var DAY_MAPPING = {
+	1: 'Monday',
+	2: 'Tuesday',
+	3: 'Wednesday',
+	4: 'Thursday',
+	5: 'Friday',
+};
+
+var dayOfWeek = new Date().getDay();
+if (!(dayOfWeek in DAY_MAPPING))
+	return;
+
+var day = DAY_MAPPING[dayOfWeek];
 
 request(SCHEDULE_URL, function(err, schedule_resp, html) {
 	if (err) {
 		console.error("Failed to load " + SCHEDULE_URL + ": ", err);
 		return;
 	}
-	var day = DAY_MAPPING[new Date().getDay()];
 	var parsedHTML = $.load(html)
 	var link = parsedHTML('a[name=' + day + ']');
 	var truck_cells = link.parent().parent().next().find('tr').eq(1).find('td');
@@ -36,10 +44,14 @@ request(SCHEDULE_URL, function(err, schedule_resp, html) {
 		attachments: attachments,
 	};
 
-	request.post({ uri: SLACK_ENDPOINT, json: true, body: message }, function(err, slack_resp, html) {
-		if (err)
-			console.error("Failed to submit to " + SLACK_ENDPOINT + ": ", message, err);
-		else
-			console.log("Submitted trucks to " + SLACK_ENDPOINT + ": ", message);
-	});
+	for (var i = 0; i < SLACK_ENDPOINTS.length; ++i)
+	{
+		var endpoint = SLACK_ENDPOINTS[i];
+		request.post({ uri: endpoint, json: true, body: message }, function(err, slack_resp, html) {
+			if (err)
+				console.error("Failed to submit to " + endpoint + ": ", message, err);
+			else
+				console.log("Submitted trucks to " + endpoint + ": ", message);
+		});
+	}
 });
