76f7cfc5dd9a52eff2dccd00e8a18e7fea7d0bfe
Jørgen P. Tjernø <jorgen@uberent.com>
2015-10-15 12:59:34 -0700

    Send each truck as its own message.
    This lets people react to individual trucks.

--- trucks.js
+++ trucks.js
@@ -8,7 +8,7 @@ var SLACK_ENDPOINTS = [
 ];
 
 if (false)
-	SLACK_ENDPOINTS = ['***REMOVED***']; // Testing.
+	SLACK_ENDPOINTS = ['***REMOVED***', '***REMOVED***']; // Testing.
 
 var SCHEDULE_URL = 'http://bellevue.com/food-truck.php';
 
@@ -26,11 +26,41 @@ if (!(dayOfWeek in DAY_MAPPING))
 
 var day = DAY_MAPPING[dayOfWeek];
 
+var getMessage = function(body)
+{
+	var base = {
+		icon_emoji: ':truck:',
+		username: 'trucks',
+	};
+
+	for (var key in body)
+		base[key] = body[key];
+	return base;
+};
+
+var processRequestQueue = function(endpoint, queue)
+{
+	if (queue.length < 1)
+	{
+		console.log("Submitted full queue to " + endpoint);
+		return;
+	}
+
+	var message = queue.shift();
+	request.post({ uri: endpoint, json: true, body: message }, function(err, slack_resp, html) {
+		if (err)
+			console.error("Failed to submit to " + endpoint + ": ", message, err);
+		else
+			processRequestQueue(endpoint, queue);
+	});
+};
+
 request(SCHEDULE_URL, function(err, schedule_resp, html) {
 	if (err) {
 		console.error("Failed to load " + SCHEDULE_URL + ": ", err);
 		return;
 	}
+
 	var parsedHTML = $.load(html)
 	var link = parsedHTML('a[name=' + day + ']');
 	var truck_cells = link.parent().parent().next().find('tr').eq(1).find('td');
@@ -38,7 +68,7 @@ request(SCHEDULE_URL, function(err, schedule_resp, html) {
 		return {
 			thumb_url: 'http://bellevue.com/' + $(el).find('img').attr('src'),
 			title: $(el).find('strong').text(),
-			title_link: 'http://bellevue.com/' + $(el).find('a').attr('href')
+			title_link: 'http://bellevue.com/' + $(el).find('a').attr('href'),
 		}
 	}).get();
 
@@ -50,31 +80,11 @@ request(SCHEDULE_URL, function(err, schedule_resp, html) {
 		return 0;
 	});
 
-	var message = {
-		icon_emoji: ':truck:',
-		username: 'trucks',
-		text: ":bell: *Food trucks* in front of Barnes & Noble today (*" + day + "*), according to <http://bellevue.com/food-truck.php|bellevue.com>",
-		attachments: attachments,
-	};
-
-	var summaryMessage = {
-		icon_emoji: ':truck:',
-		username: 'trucks',
-		text: "(Feedback &amp; feature requests can be directed to Jørgen Tjernø <mailto:jorgenpt@gmail.com|&lt;jorgenpt@gmail.com&gt;>)"
-	};
+	var messages = [getMessage({ text: ":bell: *Food trucks* in front of Barnes & Noble today (*" + day + "*), according to <http://bellevue.com/food-truck.php|bellevue.com>" })];
+	for (var i = 0; i < attachments.length; ++i)
+		messages.push(getMessage({ attachments: [attachments[i]] }));
+	messages.push(getMessage({ text: "(Feedback &amp; feature requests can be directed to Jørgen Tjernø <mailto:jorgenpt@gmail.com|&lt;jorgenpt@gmail.com&gt;>)" }));
 
 	for (var i = 0; i < SLACK_ENDPOINTS.length; ++i)
-	{
-		var endpoint = SLACK_ENDPOINTS[i];
-		request.post({ uri: endpoint, json: true, body: message }, function(err, slack_resp, html) {
-			if (err)
-				console.error("Failed to submit to " + endpoint + ": ", message, err);
-			else
-			{
-				request.post({ uri: endpoint, json: true, body: summaryMessage }, function(err, slack_resp, html) {
-					console.log("Submitted trucks to " + endpoint + ": ", message);
-				});
-			}
-		});
-	}
+		processRequestQueue(SLACK_ENDPOINTS[i], messages.slice());
 });
