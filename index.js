f05df47e8b74192c071d4aff83ad14456a844d9f
Jørgen P. Tjernø <jorgenpt@gmail.com>
2017-07-15 11:02:14 -0700

    Refactor and expose /bellevue/:day

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var express = require('express');
 
+var trucks = require('./trucks');
+
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.get('/', function(req, response) {
 	return response.sendStatus(401);
 });
+
+app.get('/bellevue/:day', function(req, response, next) {
+	trucks.fetchBellevueTrucks(req.params.day, function(err, messages)
+	{
+		if (err)
+			next(err);
+		else
+			response.send(messages);
+	});
+});
+
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
 });
--- trucks.js
+++ trucks.js
@@ -10,22 +10,6 @@ var SLACK_ENDPOINTS = [
 if (false)
 	SLACK_ENDPOINTS = ['***REMOVED***']; // Testing.
 
-var SCHEDULE_URL = 'http://bellevue.com/food-truck.php';
-
-var DAY_MAPPING = {
-	1: 'Monday',
-	2: 'Tuesday',
-	3: 'Wednesday',
-	4: 'Thursday',
-	5: 'Friday',
-};
-
-var dayOfWeek = new Date().getDay();
-if (!(dayOfWeek in DAY_MAPPING))
-	return;
-
-var day = DAY_MAPPING[dayOfWeek];
-
 var getMessage = function(body)
 {
 	var base = {
@@ -55,36 +39,70 @@ var processRequestQueue = function(endpoint, queue)
 	});
 };
 
-request(SCHEDULE_URL, function(err, schedule_resp, html) {
-	if (err) {
-		console.error("Failed to load " + SCHEDULE_URL + ": ", err);
+var BELLEVUE_SCHEDULE_URL = 'http://bellevue.com/food-truck.php';
+var fetchBellevueTrucks = function(dayOfWeek, callback)
+{
+	var DAY_MAPPING = {
+		1: 'Monday',
+		2: 'Tuesday',
+		3: 'Wednesday',
+		4: 'Thursday',
+		5: 'Friday',
+	};
+
+	if (!(dayOfWeek in DAY_MAPPING))
 		return;
-	}
 
-	var parsedHTML = $.load(html)
-	var link = parsedHTML('a[name=' + day + ']');
-	var truck_cells = link.parent().parent().next().find('tr').eq(1).find('td');
-	var attachments = truck_cells.map(function(i, el) {
-		return {
-			thumb_url: 'http://bellevue.com/' + $(el).find('img').attr('src'),
-			title: $(el).find('strong').text(),
-			title_link: 'http://bellevue.com/' + $(el).find('a').attr('href'),
+	var day = DAY_MAPPING[dayOfWeek];
+	request(BELLEVUE_SCHEDULE_URL, function(err, schedule_resp, html) {
+		if (err) {
+			callback(err, null);
+			return;
 		}
-	}).get();
 
-	attachments.sort(function(a, b) {
-	  	if (a.title < b.title)
-		    return -1;
-		if (a.title > b.title)
-		    return 1;
-		return 0;
+		var parsedHTML = $.load(html)
+		var link = parsedHTML('a[name=' + day + ']');
+		var truck_cells = link.parent().parent().next().find('tr').eq(1).find('td');
+		var attachments = truck_cells.map(function(i, el) {
+			return {
+				thumb_url: 'http://bellevue.com/' + $(el).find('img').attr('src'),
+				title: $(el).find('strong').text(),
+				title_link: 'http://bellevue.com/' + $(el).find('a').attr('href'),
+			}
+		}).get();
+
+		attachments.sort(function(a, b) {
+			if (a.title < b.title)
+				return -1;
+			if (a.title > b.title)
+				 return 1;
+			return 0;
+		});
+
+		var messages = [getMessage({ text: ":bell: *Food trucks* in front of Barnes & Noble today (*" + day + "*), according to <http://bellevue.com/food-truck.php|bellevue.com>" })];
+		for (var i = 0; i < attachments.length; ++i)
+			messages.push(getMessage({ attachments: [attachments[i]] }));
+		messages.push(getMessage({ text: "(Feedback &amp; feature requests can be directed to Jørgen Tjernø <mailto:jorgenpt@gmail.com|&lt;jorgenpt@gmail.com&gt;>)" }));
+
+		callback(null, messages);
 	});
+};
 
-	var messages = [getMessage({ text: ":bell: *Food trucks* in front of Barnes & Noble today (*" + day + "*), according to <http://bellevue.com/food-truck.php|bellevue.com>" })];
-	for (var i = 0; i < attachments.length; ++i)
-		messages.push(getMessage({ attachments: [attachments[i]] }));
-	messages.push(getMessage({ text: "(Feedback &amp; feature requests can be directed to Jørgen Tjernø <mailto:jorgenpt@gmail.com|&lt;jorgenpt@gmail.com&gt;>)" }));
+module.exports = {
+	fetchBellevueTrucks: fetchBellevueTrucks,
+};
 
-	for (var i = 0; i < SLACK_ENDPOINTS.length; ++i)
-		processRequestQueue(SLACK_ENDPOINTS[i], messages.slice());
-});
+if (require.main == module)
+{
+	fetchBellevueTrucks(new Date().getDay(), function(err, messages) {
+		if (err)
+		{
+			console.error("Failed to load " + BELLEVUE_SCHEDULE_URL + ": ", err);
+		}
+		else
+		{
+			for (var i = 0; i < SLACK_ENDPOINTS.length; ++i)
+				processRequestQueue(SLACK_ENDPOINTS[i], messages.slice());
+		}
+	});
+}
